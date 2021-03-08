ae783e3079df96f7b4468cfd1efac58ece18be67
Alexander <46220328+CactusTactus@users.noreply.github.com>
2021-03-08 09:04:44 +0300

    Update feature_script.py (#1)

--- feature_script.py
+++ feature_script.py
@@ -1,3 +1,4 @@
 for i in range(10): print("Feature")
 print("New feature 1.0.1")
-print("Edited on GitKraken for pull-request")
+print("Edited on GitKraken for pull-request #2")
+print("Update feature for pull-request #1")
